3e432409877eadc243a9d886506b3104e9913eeb
Nduka-Nwagbo <69093339+Nduka-Nwagbo@users.noreply.github.com>
2023-02-06 11:08:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function updateClock(){
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
     let ampm = "AM"
-    if (h >= 0 >= 12 < 24) {
+    if (h >= 12) {
         ampm = "PM"
     }
-
+    
     h = h<10 ? "0" + h : h;
     m = m<10 ? "0" + m : m;
     s = s<10 ? "0" + s : s;
@@ -24,12 +24,26 @@ function updateClock(){
     setTimeout(() => {
         updateClock()
     }, 1000)
-    if (h >= 6 <= 18 ) {
+    if (h < 18) {
       document.getElementById("body").className = "background2";
       hourElement.className = "time2";
       minuteElement.className = "time2";
       secondElement.className = "time2";
       timeofdayElement.className = "ampm2";
+    }
+    if (h < 7) {
+        document.getElementById("body").className = "body";
+        hourElement.className = "time";
+        minuteElement.className = "time";
+        secondElement.className = "time";
+        timeofdayElement.className = "ampm";
+    }
+    if (h > 18) {
+        document.getElementById("body").className = "body";
+        hourElement.className = "time";
+        minuteElement.className = "time";
+        secondElement.className = "time";
+        timeofdayElement.className = "ampm";
     }
 }
 updateClock();
